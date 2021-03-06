508718c1b94080bc1a861991c4da64e64dcba5f5
SallMarta <63784694+SallMarta@users.noreply.github.com>
2021-03-06 23:29:32 +0700

    commit

--- R_Programming_1/R_Programming_1.R
+++ R_Programming_1/R_Programming_1.R
@@ -1,1 +1,16 @@
-123 <-A
+values = sample(1:1000, 100, replace = FALSE)
+
+G = matrix(values, nrow = 10)
+
+H = t(G)
+
+J = G+H
+
+det(G)
+det(H)
+det(J)
+
+K <- list(G, J)
+do.call(cbind, lapply(K, function(x) x[, c(1, 2, 3, 4, 5)]))
+
+G%*%(solve(G))
